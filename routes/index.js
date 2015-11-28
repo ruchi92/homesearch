96dd71225b273a0ade2d4364fdfff74f9415c303
Mallika <mallikak2709@gmail.com>
2015-11-28 13:10:42 -0800

    Index page integrated

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { langchange: langchange });
 });
+
+var langchange = {
+    english: {
+        tools: "tools",
+        message: "View in Spanish",
+        aref: "http://localhost:3000/home/spanish"
+    }, spanish: {
+        tools: "herramientas",
+        message: "Ver en Inglés",
+        aref: "http://localhost:3000/home"
+    }
+}
 
 module.exports = router;
